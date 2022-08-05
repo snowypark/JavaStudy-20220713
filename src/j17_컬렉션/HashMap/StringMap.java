a9c4a79efb9866771fbe430a9624a590bf46d9f4
snowypark <partsnowy@gmail.com>
2022-08-05 09:08:41 +0900

    챕터 17.컬렉션

--- src/j17_컬렉션/HashMap/StringMap.java
+++ src/j17_컬렉션/HashMap/StringMap.java
@@ -7,42 +7,27 @@ public class StringMap {
 
 	public static void main(String[] args) {
 		
-		// key , value
-		// 컬렉션, 인터페이스 상속X
 		HashMap<Integer, String> strMap = new HashMap<Integer, String>();
 		
 		HashMap<String, String> strMap2 = new HashMap<String, String>();
 		
+		strMap.put(10, "java1");
+		strMap.put(20, "java2");
+		strMap.put(30, "java3");
+		strMap.put(40, "java4");
 		
-		//Map.put 으로 값 추가
-		strMap.put(1, "java");
-		strMap.put(1, "java2");
-		strMap.put(1, "java3");
-		strMap.put(1, "java4");
-		strMap.put(2, "java4");
-		strMap.put(3, "java4");
-		strMap.put(4, "java4");
-		
-		// key 값 중복 X
 		System.out.println(strMap);
 		
-		// value 중복 O
-		strMap2.put("A", "python");
-		strMap2.put("B", "python");
-		strMap2.put("C", "python");
-		strMap2.put("D", "python");
+		strMap2.put("a", "python");
+		strMap2.put("b", "python");
+		strMap2.put("c", "python");
+		strMap2.put("d", "python");
 		
 		System.out.println(strMap2);
 		
-		// for-each 반복 불가능
-		// iterator 사용
-		
-		// Index (X) -> key value
 		strMap.get(10);
 		
-		//keySet = return
 		Iterator<Integer> irKey = strMap.keySet().iterator();
-		
 		while(irKey.hasNext()) {
 			System.out.println(strMap.get(irKey.next()));
 		}
@@ -52,9 +37,13 @@ public class StringMap {
 			System.out.println(arg.getKey());
 			System.out.println(arg.getValue());
 		});
-
-	 
-		
 	}
 
 }
+
+
+
+
+
+
+
--- src/j17_컬렉션/HashSet/StringHashSet.java
+++ src/j17_컬렉션/HashSet/StringHashSet.java
@@ -6,52 +6,34 @@ import java.util.Iterator;
 public class StringHashSet {
 
 	public static void main(String[] args) {
-
 		HashSet<String> set = new HashSet<String>();
-		// 입력 순서에 상관없이 출력. 자료구조 -> 해시 공부
-
 		set.add("java");
-		set.add("java1");
+		set.add("java");
+		set.add("java");
+		set.add("java");
+		set.add("python");
 		set.add("python");
 		set.add("python");
 		set.add("python");
 		set.add("1");
-
+		set.add("2");
+		
 		System.out.println(set);
-
-		Iterator<String> ir = set.iterator();
-		ir.hasNext();
-
-//		System.out.println(ir.hasNext()); // 다음을 확인
-//		System.out.println(ir.next());
-//		System.out.println(ir.hasNext());
-//		System.out.println(ir.next());
-//		System.out.println(ir.hasNext());
-//		System.out.println(ir.next());
-//		System.out.println(ir.hasNext());
-//		System.out.println(ir.next());		
-//		System.out.println(ir.hasNext()); // 다 비었다.
-
-		System.out.println();
-		System.out.println(set);
-
 		// 값 조회
-		while (ir.hasNext()) {
+		Iterator<String> ir = set.iterator();
+		while(ir.hasNext()) {
 			String str = ir.next();
-			if (str.equals("java")) {
-				System.out.println(str);
+			if(str.equals("java")) {
+				System.out.println(str);				
 			}
 		}
-		
-		
-		
-		
-		// set의 수정 : 값 삭제 -> add로 새 값 추가.
-		// 특정 값의 변경 불가능.
-		
-		
-		
-		
+
 	}
 
 }
+
+
+
+
+
+
