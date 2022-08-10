c1bd15bc142e448d2d5092f24a2c75de9366f982
snowypark <partsnowy@gmail.com>
2022-08-10 12:46:14 +0900

    no message

--- src/j17_컬렉션/user/UserMain.java
+++ src/j17_컬렉션/user/UserMain.java
@@ -7,30 +7,30 @@ import java.util.Scanner;
 import java.util.Set;
 
 public class UserMain {
-	
-	
+		
+		
 					 // key ,  value
 	public static Map<String, String> addUser() {
 		// "username" , scanner 입력으로 key 값 저장
-
+		
 		Scanner scanner = new Scanner(System.in);
-
+		
 		Map<String, String> user = new HashMap<String, String>();
-
+		
 		System.out.print("아이디: ");
 		user.put("username", scanner.nextLine());
 		// String " " Key값
-
+		
 		System.out.print("비밀번호: ");
 		user.put("password", scanner.nextLine());
-
+		
 		System.out.print("이름: ");
 		user.put("name", scanner.nextLine());
-
+		
 		return user;
 		// 저장된 값을 user로 반환 
-	}
-		
+	}	
+			
 	public static void main(String[] args) {
 		
 		Map<String, String> user = new HashMap<String, String>();
@@ -38,9 +38,9 @@ public class UserMain {
 		// set in ( map<> )
 		Set<Map<String, String>> userSet = new HashSet<Map<String, String>>();
 		// set = 값을 저장 = 입력
-
+		
 		// addUser에 저장한 양식을 입력
-
+			
 		userSet.add(addUser());
 		userSet.add(addUser());
 		userSet.add(addUser());
@@ -53,9 +53,9 @@ public class UserMain {
 			System.out.println("비밀번호: " + map.get("password"));
 			System.out.println("이름: " + map.get("name"));
 		});
-
+		
 		// key값 String으로 출력하기 때문에 "username"이 오타날 경우 null
-
+		
 		// Map = interface 기반
 		// hashmap = map을 구현
 	}
